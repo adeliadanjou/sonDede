a31d07aad6ca344aef8ea9b2b9409569045a54a1
Sonia Bravo Collado <sbravoco@everis.com>
2019-10-22 16:03:52 +0200

    navbar material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,34 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PeliculaComponent } from './pelicula/pelicula.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 const routes: Routes = [
 { path:"",redirectTo:"login", pathMatch:"full"},
 { path:"login", component: LoginComponent},
 { path:"signup", component: SignupComponent},
+{ path:"peliculas", component: PeliculasComponent},
 
 ]
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    PeliculaComponent,
+    PeliculasComponent,
+    NavbarComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +44,13 @@ const routes: Routes = [
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
